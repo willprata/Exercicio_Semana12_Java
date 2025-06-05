55f790459cf7e09d4baea9012250ba49d81f6512
WillPrata <wilson.prata@soulasalle.com.br>
2025-06-05 19:21:48 -0300

    Create Retangulo.java

---
+++ Exercicio2/Formas/Retangulo.java
@@ -0,0 +1,37 @@
+package formas;
+
+public class Retangulo implements FormaGeometrica {
+    private double largura;
+    private double altura;
+
+    public Retangulo(double largura, double altura) {
+        this.largura = largura;
+        this.altura = altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+}
